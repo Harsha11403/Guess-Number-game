d9d9784d24b85c04b6cc774abc69dcd3d1d77b47
unknown <chennaharsha11403@gmail.com>
2023-06-19 20:23:24 +0530

    Initial  commit

---
+++ guess.js
@@ -0,0 +1,28 @@
+let randomNumber=Math.ceil(Math.random()*100);
+console.log(randomNumber);
+let inputText=document.getElementById("userInput");
+let gameResult=document.getElementById("gameOutput");
+function checkGuess()
+{
+    let inputNumber=parseInt(inputText.value);
+    if(inputNumber > randomNumber)
+    {
+        gameResult.textContent="Too High! Try Again...";
+        gameResult.style.backgroundColor="Red";
+    }
+    else if(inputNumber < randomNumber)
+    {
+        gameResult.textContent="Too Low! Try Again...";
+        gameResult.style.backgroundColor="Red";
+    }
+    else if(inputNumber === randomNumber)
+    {
+        gameResult.textContent="Hurray!!!You  Win..."
+        gameResult.style.backgroundColor="green";
+    }
+    else{
+        gameResult.textContent="Invalid Input.Try Again!!!"
+        gameResult.style.backgroundColor="red";
+    }
+    
+}
